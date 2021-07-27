70d07765e46804b1a57f365500b75f70ab7d68a9
henrfarias <henrique.biasibetti@gmail.com>
2021-07-27 11:45:09 -0300

    chore: config models

--- app/Models/Transaction.ts
+++ app/Models/Transaction.ts
@@ -1,13 +1,26 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import User from './User'
 
 export default class Transaction extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
+  @column()
+  public sender_id: string
+
+  @column()
+  public receiver_id: string
+
+  @column()
+  public value: number
+
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime
+  @belongsTo(() => User)
+  public receiver: BelongsTo<typeof User>
+
+  @belongsTo(() => User)
+  public sender: BelongsTo<typeof User>
 }
--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,13 +1,44 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { v4 as uuid4 } from 'uuid'
+import {
+  BaseModel,
+  beforeCreate,
+  beforeSave,
+  column,
+  HasMany,
+  hasMany,
+} from '@ioc:Adonis/Lucid/Orm'
+import Hash from '@ioc:Adonis/Core/Hash'
+import Transaction from './Transaction'
 
 export default class User extends BaseModel {
   @column({ isPrimary: true })
-  public id: number
+  public id: string
+
+  @beforeCreate()
+  public static assignUuid(user: User) {
+    user.id = uuid4()
+  }
+
+  @column()
+  public email: string
+
+  @column({ serializeAs: null })
+  public password: string
+
+  @beforeSave()
+  public static async hashPassword(user: User) {
+    if (user.$dirty.password) {
+      user.password = await Hash.make(user.password)
+    }
+  }
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @hasMany(() => Transaction)
+  public transactions: HasMany<typeof Transaction>
 }
